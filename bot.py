f973245390a731fc7f339b9c9b98f30056ef566f
victor141516 <yomellamovictor@gmail.com>
2017-10-05 15:57:59 +0200

    General improvements

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 from flask import Flask, request, make_response
-import json
 import os
 import requests
 import telebot
@@ -11,24 +10,37 @@ bot = telebot.TeleBot(API_TOKEN)
 TELEGRAM_FILE_URL = "https://api.telegram.org/file/bot{token}/".format(token=API_TOKEN) + "{file_path}"
 
 def google_url_shorten(url):
-   req_url = 'https://www.googleapis.com/urlshortener/v1/url?key=' + GOOGLE_URL_SHORTEN_API
-   payload = {'longUrl': url}
-   headers = {'content-type': 'application/json'}
-   r = requests.post(req_url, data=json.dumps(payload), headers=headers)
-   resp = json.loads(r.text)
-   return resp['id']
+   req_url = "https://www.googleapis.com/urlshortener/v1/url?key={api_key}".format(api_key=GOOGLE_URL_SHORTEN_API)
+   r = requests.post(req_url, json={'longUrl': url}, headers={'content-type': 'application/json'})
+   return r.json()['id']
 
-@bot.message_handler(content_types=['document'])
+@bot.message_handler(content_types=['text', 'audio', 'document', 'photo', 'sticker', 'video', 'video_note', 'voice', 'location', 'contact', 'new_chat_members', 'left_chat_member', 'new_chat_title', 'new_chat_photo', 'delete_chat_photo', 'group_chat_created', 'supergroup_chat_created', 'channel_chat_created', 'migrate_to_chat_id', 'migrate_from_chat_id', 'pinned_message'])
 def get_link(message):
-    if (message.document.file_size > 20 * 1024 * 1024):
-        bot.reply_to(message, "This file is too big. Max file size is 20MB")
+    # Get message whatever type it is
+    document = message.__getattribute__(message.content_type)[1] if type(message.__getattribute__(message.content_type)) is type([]) else message.__getattribute__(message.content_type)
+
+    # Check if it has file_id (some types doesn't so them can't be linked)
+    if (not hasattr(document, 'file_id')):
+        bot.reply_to(message, "That content can't be linked")
         return
 
+    # Try to generate the link. It may fail because file size mainly, but may there be other reasons
     try:
-        url = TELEGRAM_FILE_URL.format(file_path=bot.get_file(message.document.file_id).file_path)
+        url = TELEGRAM_FILE_URL.format(file_path=bot.get_file(document.file_id).file_path)
+    except Exception as e:
+        error_message = str(e).split('"description":"')[1].split('"')[0]
+        if (error_message == "Bad Request: file is too big"):
+            bot.reply_to(message, "This file is too big. Max file size is 20MB")
+        else:
+            bot.reply_to(message, error_message[0].upper() + error_message[1:])
+        return
+
+    # Try to short the link. May fail due Google policies
+    try:
         bot.reply_to(message, google_url_shorten(url))
     except Exception as e:
         bot.reply_to(message, url)
+        return
 
 
 @server.route("/bot", methods=['POST'])
